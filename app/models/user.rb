23ef19983a60303944b9008d5d57c9ecb8900d79
tkmprogram <takumi.kojo.2@gmail.com>
2022-02-03 11:31:15 +0900

    userモデル　バリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presance: true
+  validates :last_name, presance: true
+  validates :first_name, presance: true
+  validates :last_name_katakana, presance: true
+  validates :first_name_katakana, presance: true
+  validates :birthday, presance: true
 end
