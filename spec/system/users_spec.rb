a76ed63160cee52984e31f76df1821f5429e8792
tkmprogram <takumi.kojo.2@gmail.com>
2022-02-04 22:22:01 +0900

    バリデーションとか

---
+++ spec/system/users_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "ユーザー新規登録", type: :system do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+  context 'ユーザー登録ができるとき' do
+    it '正しい情報を入力すればユーザー新規登録ページに移動するとき' do
+      basic_pass root_path
+      expect(page).to have_content('新規登録')
+      visit new_user_registration_path
+      fill_in 'nickname', with: @user.nickname
+      fill_in 'email', with: @user.email
+      fill_in 'password', with: @user.password
+      fill_in 'password-confirmation', with: @user.password_confirmation
+      fill_in 'last-name', with: @user.last_name
+      fill_in 'first-name', with: @user.first_name
+      fill_in 'last-name-kana', with: @user.last_name_katakana
+      fill_in 'first-name-kana', with: @user.first_name_katakana
+      
+      expect{
+        find('input[name=会員登録]').click
+      }.to change { User.count }.by{1}
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('ログアウト')
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
+    end
+  end
+end
