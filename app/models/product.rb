b7ee27d248dccf78e10fd449adb78a2d42c2abe4
tkmprogram <takumi.kojo.2@gmail.com>
2022-02-07 12:38:08 +0900

    モデルとテーブルの作成

---
+++ app/models/product.rb
@@ -0,0 +1,11 @@
+class Product < ApplicationRecord
+  validates :category_id, presence: true
+  validates :situation_id, presence:true
+  validates :shipping_charge_id, presence:true
+  validates :prefecture_id, presence: true
+  validates :send_day_id, presence: true
+  validates :price, presence: true
+  validates :title, presence: true
+  validates :explanation, presence: true
+  validates :user, presence: true
+end
---
+++ db/migrate/20220207031151_create_products.rb
@@ -0,0 +1,16 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.integer :category_id, null: false
+      t.integer :situation_id, null: false
+      t.integer :shipping_charge_id, null: false
+      t.integer :prefecture_id, null: false
+      t.integer :send_day_id, null: false
+      t.integer :price, null: false
+      t.string :title, null: false
+      t.text :explanation, null: false
+      t.references :user, null:false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
