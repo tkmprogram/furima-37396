58f9feaca03ddefafec232a5bfcbec4a931d82da
tkmprogram <takumi.kojo.2@gmail.com>
2022-02-11 17:20:57 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,7 +38,7 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.user_id == current_user.id
-      item.destroy
+      @item.destroy
       redirect_to root_path
     else
       redirect_to :index
